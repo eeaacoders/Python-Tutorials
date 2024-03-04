f24419d7775072b7d52e6bcfa600fcc61b31a803
eeaacoders <codeea086@gmail.com>
2024-03-04 18:28:16 +0500

    new

---
+++ Project1/gitadd.py
@@ -0,0 +1,22 @@
+print("Hello")
+
+list = [1,2,3,4,5]
+
+index = 0
+
+for i in list:
+    print(i)
+    if i == 2:
+        print("it is 2.")
+    if index == 2:
+        print("it is t number2.")
+    index= index + 1
+
+num = int(input("Enter the number: "))
+
+fact = 1
+
+for i in range(1,num+1):
+    fact = fact * i
+
+print("The factorial of the number is: ",fact)
